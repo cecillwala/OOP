e7dfbd065a381d6c8cde718f72625955154a212d
AaronKipchirchir <151519765+AaronKipchirchir@users.noreply.github.com>
2023-11-20 16:14:19 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,68 +1,71 @@
 using System;
-using System.IO;
-using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace waterbillcalculator
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Enter your income in Ksh: ");
-        double income = Convert.ToDouble(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"-----------------------------------------------
+ Welcome to the Monthly Water Bill Calculator!
+-----------------------------------------------");
+            bool isRunning = true;
+            while (isRunning)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    // Input water consumption
+                    Console.Write("Please enter the water consumption in m^3: ");
+                    double waterConsumption = double.Parse(Console.ReadLine());
 
-        double tax = CalculatePAYE(income);
+                    // Input usage type
+                    Console.WriteLine("Please enter the usage type:");
+                    Console.WriteLine("1. Residential.");
+                    Console.WriteLine("2. Commercial.");
+                    Console.WriteLine("3. Industrial.");
+                    Console.Write("Select choice: ");
+                    int usageType = int.Parse(Console.ReadLine());
+                    string usage = "residential";
+                    double Rate = 0;
 
-        Console.WriteLine("Your PAYE tax is: " + tax.ToString("C", CultureInfo.CreateSpecificCulture("en-KE")));
+                    // Determine rate based on usage type
+                    if (usageType == 1)
+                    {
+                        Rate = 12.50;
+                        usage = "residential";
+                    }
+                    else if (usageType == 2)
+                    {
+                        Rate = 15.75;
+                        usage = "commercial";
+                    }
+                    else if (usageType == 3)
+                    {
+                        Rate = 18.90;
+                        usage = "industrial";
+                    }
 
-        // Generate PAYE Certificate or Report
-        GeneratePAYECertificateOnDesktop(income, tax);
+                    // Calculate total bill and display breakdown
+                    double totalBill = waterConsumption * Rate;
+                    Console.WriteLine($"Breakdown for {usage} usage:");
+                    Console.WriteLine($"Water Consumption: {waterConsumption} m^3");
+                    Console.WriteLine($"Rate: {Rate} KES/m^3");
+                    Console.WriteLine($"Total Bill: {totalBill} KES");
+                    Console.Write("Press enter to exit.");
+                }
+                catch
+                {
+                    Console.WriteLine("Wrong input. Try again");
+                    continue;
+                }
+                Console.ReadKey();
+                
+            }
+        }
     }
 
-    static double CalculatePAYE(double income)
-    {
-        double tax = 0;
-
-        if (income <= 24000)
-        {
-            tax = income * 0.1;
-        }
-        else if (income <= 40000)
-        {
-            tax = 2400 + (income - 24000) * 0.15;
-        }
-        else if (income <= 60000)
-        {
-            tax = 4800 + (income - 40000) * 0.2;
-        }
-        else if (income <= 100000)
-        {
-            tax = 8800 + (income - 60000) * 0.25;
-        }
-        else if (income <= 150000)
-        {
-            tax = 18800 + (income - 100000) * 0.3;
-        }
-        else if (income <= 250000)
-        {
-            tax = 38800 + (income - 150000) * 0.35;
-        }
-        else
-        {
-            tax = 78800 + (income - 250000) * 0.4;
-        }
-
-        return tax;
-    }
-
-    static void GeneratePAYECertificateOnDesktop(double income, double tax)
-    {
-        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        string certificatePath = Path.Combine(desktopPath, "PAYECertificate.txt");
-
-        string certificate = $"PAYE Certificate\n\nIncome: {income:C}\nPAYE Tax: {tax:C}\n\nThank you for your contribution to taxes.";
-
-        // Save the certificate to the desktop
-        File.WriteAllText(certificatePath, certificate);
-
-        Console.WriteLine($"PAYE Certificate generated and saved to {certificatePath}");
-    }
-}
